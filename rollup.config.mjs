9016da6a01c837adca6aaa16c7a40ee861c65756
Sergey Andreev <elenik1994@gmail.com>
2024-09-04 22:21:39 +0300

    config: init settings

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
 import html from '@rollup/plugin-html'
-import css from "rollup-plugin-import-css"
+import css from 'rollup-plugin-import-css'
 
 export default {
   input: 'src/generator.mjs',
